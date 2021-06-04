2bab7c9de18a72f2051ebc6de06a6833fe3c10d2
Anna Akopyan <anchello@yandex.ru>
2021-06-04 12:23:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,36 @@
+﻿// ConsoleApplication3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <ctime>
+#include <time.h>
+
+
+
+int main()
+{
+    const int N = 3;
+    int array[N][N];
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            array[i][j] = i + j;
+
+            std::cout << array[i][j];
+        }
+        std::cout << "\n";
+    }
+
+    time_t t;
+    time(&t);
+    int x = (localtime(&t)->tm_mday) % N;
+
+    time_t now = time(0);
+    tm* localtm = localtime(&now);
+    std::cout << "The local date and time is: " << asctime(localtm);
+
+ 
+    
+}
